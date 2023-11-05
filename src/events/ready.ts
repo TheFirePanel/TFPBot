902c251f9ada18cb9111e0e2ff271a7b2e8158ff
DrPepperG <josephcrain14@gmail.com>
2023-11-04 21:21:07 -0400

    remove async on ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,7 +4,7 @@ import color from 'chalk';
 export default {
     name: Events.ClientReady,
     once: true,
-    async execute(client: Client) {
+    execute(client: Client) {
         if (!client) return;
         console.log(color.bold.magenta(`Ready! Logged in as ${color.bgCyan(client.user!.tag)}`));
     },
