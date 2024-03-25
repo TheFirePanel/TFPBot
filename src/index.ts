e18e5c7477beb43e8dc0b92f9410b526f216d84c
zacharyfleck <zfleck@clinetic.com>
2024-03-25 17:58:32 -0400

    2.0.0

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { config as dotenv } from 'dotenv';
 dotenv();
 
 // This is automatically updated when npm version is ran successfully
-process.env.version = '1.6.0';
+process.env.version = '2.0.0';
 
 // Default imports
 import {
